0d8b2cf2c2120d419048698c3d1c88286c7b37be
sihyunjojo <si4018@naver.com>
2025-03-18 23:56:41 +0900

    feat: member 생성 및 저장 로직 추가

---
+++ src/main/java/org/c4marathon/assignment/service/MemberService.java
@@ -0,0 +1,29 @@
+package org.c4marathon.assignment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.domain.Member;
+import org.c4marathon.assignment.dto.member.MemberRegistrationRequestDto;
+import org.c4marathon.assignment.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long registerMember(MemberRegistrationRequestDto request) {
+        Member member = Member.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(request.password())
+                .build();
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+}
