2b5687485df21b49a25b75e578e449a64bd570a5
sihyunjojo <si4018@naver.com>
2025-03-19 14:54:16 +0900

    feat: 적금 계촤 생성 추가 앤드포인트 이름 변경

--- src/main/java/org/c4marathon/assignment/controller/MemberAccountController.java
+++ src/main/java/org/c4marathon/assignment/controller/MemberAccountController.java
@@ -1,8 +1,11 @@
 package org.c4marathon.assignment.controller;
 
+import org.c4marathon.assignment.dto.ApiResponse;
+import org.c4marathon.assignment.dto.account.AccountDto;
 import org.c4marathon.assignment.dto.member.MemberRegistrationRequestDto;
 import org.c4marathon.assignment.service.MemberAccountService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/member-accounts")
 @RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberAccountController {
     private final MemberAccountService memberAccountService;
 
-    @PostMapping("/register")
-    public ResponseEntity<Long> registerMemberWithAccount(@RequestBody MemberRegistrationRequestDto request) {
+    @PostMapping
+    public ResponseEntity<ApiResponse<Long>> registerMemberWithAccount(@RequestBody MemberRegistrationRequestDto request) {
         Long memberId = memberAccountService.registerMemberWithAccount(request);
-        return ResponseEntity.ok(memberId);
+        return ResponseEntity.ok(ApiResponse.res(201, "회원 등록 및 메인 계좌 생성 완료", memberId));
+    }
+
+    @PostMapping("/{memberId}/accounts/saving")
+    public ResponseEntity<ApiResponse<AccountDto>> createSavingAccount(@PathVariable Long memberId) {
+        AccountDto savingAccount = memberAccountService.registerSavingAccount(memberId);
+        return ResponseEntity.ok(ApiResponse.res(201, "적금 계좌 생성 완료", savingAccount));
     }
 }
