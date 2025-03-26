10a12c9482cd5f6d56e30a499fa9daddeb3f4fc7
sihyunjojo <si4018@naver.com>
2025-03-26 23:47:33 +0900

    refactor: 계좌 타입 삭제

--- src/main/java/org/c4marathon/assignment/dto/account/AccountDto.java
+++ src/main/java/org/c4marathon/assignment/dto/account/AccountDto.java
@@ -1,10 +1,9 @@
 package org.c4marathon.assignment.dto.account;
 
-import org.c4marathon.assignment.domain.Account;
-import org.c4marathon.assignment.domain.enums.AccountType;
+import org.c4marathon.assignment.domain.account.Account;
 
-public record AccountDto(Long id, Long memberId, Long balance, AccountType accountType) {
+public record AccountDto(Long id, Long memberId, Long balance) {
     public AccountDto(Account account) {
-        this(account.getId(), account.getMember().getId(), account.getBalance(), account.getAccountType());
+        this(account.getId(), account.getMember().getId(), account.getBalance());
     }
 }
