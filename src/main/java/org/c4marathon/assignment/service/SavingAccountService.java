70731d17e0a847e1da176887c78b4e0104d9ae48
sihyunjojo <si4018@naver.com>
2025-04-16 16:52:29 +0900

    feat: 성능 비교를 위한 동일한 기능 추가

--- src/main/java/org/c4marathon/assignment/service/SavingAccountService.java
+++ src/main/java/org/c4marathon/assignment/service/SavingAccountService.java
@@ -1,12 +1,11 @@
 package org.c4marathon.assignment.service;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.c4marathon.assignment.domain.Member;
 import org.c4marathon.assignment.domain.account.MainAccount;
 import org.c4marathon.assignment.domain.account.SavingAccount;
 import org.c4marathon.assignment.dto.account.AccountDto;
+import org.c4marathon.assignment.repository.EntityReferenceRepository;
 import org.c4marathon.assignment.repository.SavingAccountRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,20 +14,33 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SavingAccountService {
 
-	@PersistenceContext
-	private EntityManager entityManager;
-
 	private final SavingAccountRepository savingAccountRepository;
+	private final EntityReferenceRepository entityReferenceRepository;
 
 	@Transactional
 	public AccountDto createSavingAccountForMember(Long memberId, Long mainAccountId) {
-		Member memberProxy = entityManager.getReference(Member.class, memberId);
-		MainAccount mainAccountProxy = entityManager.getReference(MainAccount.class, mainAccountId);
+		Member memberProxy = entityReferenceRepository.getMemberReference(memberId);
+		MainAccount mainAccountProxy = entityReferenceRepository.getMainAccountReference(mainAccountId);
 
 		SavingAccount savingAccount = SavingAccount.builder()
 			.member(memberProxy)
 			.balance(0L)
 			.mainAccount(mainAccountProxy)
+			.build();
+
+		savingAccountRepository.save(savingAccount);
+
+		return new AccountDto(savingAccount);
+	}
+
+	@Transactional
+	public AccountDto createSavingAccountForMember(Long memberId, MainAccount mainAccount) {
+		Member memberProxy = entityReferenceRepository.getMemberReference(memberId);
+
+		SavingAccount savingAccount = SavingAccount.builder()
+			.member(memberProxy)
+			.balance(0L)
+			.mainAccount(mainAccount)
 			.build();
 
 		savingAccountRepository.save(savingAccount);
