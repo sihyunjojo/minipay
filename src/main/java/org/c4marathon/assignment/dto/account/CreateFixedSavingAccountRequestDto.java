c82a9e7b6bc1bd7dd8123bca1e6c33a16486344a
sihyunjojo <si4018@naver.com>
2025-04-27 16:54:18 +0900

    docs: 잘못된 파라미터 설명 변경

--- src/main/java/org/c4marathon/assignment/dto/account/CreateFixedSavingAccountRequestDto.java
+++ src/main/java/org/c4marathon/assignment/dto/account/CreateFixedSavingAccountRequestDto.java
@@ -4,6 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "정기 적금 계좌 생성 요청 DTO")
 public record CreateFixedSavingAccountRequestDto(
-	@Schema(description = "가입 금액 (원)", example = "100000")
+	@Schema(description = "정기 입금 금액(원)", example = "1000")
 	Long subscribedDepositAmount
 ) {}
