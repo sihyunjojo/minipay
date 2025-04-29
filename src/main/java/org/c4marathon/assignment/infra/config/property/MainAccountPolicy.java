a0106776ba5e961997d067bfa3d1aa0ac791d378
sihyunjojo <si4018@naver.com>
2025-04-29 21:00:49 +0900

    refactor: 기존 계좌 정책 설정을 AccountPolicyProperties로 도입으로 yml 매핑 최적화

--- src/main/java/org/c4marathon/assignment/infra/config/property/MainAccountPolicy.java
+++ src/main/java/org/c4marathon/assignment/infra/config/property/MainAccountPolicy.java
@@ -4,20 +4,19 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationProperties(prefix = "business.rule.account")
 @Getter
 @Setter
-public class MainAccountPolicyProperties {
+public class MainAccountPolicy {
 
 	// 💡 'business.rule.account.main-daily-limit' 값이 yml에 없을 경우 3000000으로 fallback (기본값)
 	@NotNull
 	private Long mainDailyLimit = 3_000_000L;
+
 	@NotNull
 	private Long chargeUnit = 10_000L;
+
+	@NotNull
+	private String accountPrefix = "01";
 
 	public Long getRoundedCharge(Long shortfall) {
 		return ((shortfall + chargeUnit - 1) / chargeUnit) * chargeUnit;
