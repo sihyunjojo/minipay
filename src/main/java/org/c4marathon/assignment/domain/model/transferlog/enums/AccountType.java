11890b1b659fca9cb560b76d33f2b0b7bba25eee
sihyunjojo <si4018@naver.com>
2025-04-29 16:52:29 +0900

    feat: 로그에 필요한 각 계좌의 타입 추가

---
+++ src/main/java/org/c4marathon/assignment/domain/model/transferlog/enums/AccountType.java
@@ -0,0 +1,7 @@
+package org.c4marathon.assignment.domain.model.transferlog.enums;
+
+public enum AccountType {
+	MAIN_ACCOUNT,     // 메인 계좌
+	SAVING_ACCOUNT,   // 적금 계좌
+	EXTERNAL_ACCOUNT  // 외부 계좌 (ex. 외부 충전/출금)
+}
