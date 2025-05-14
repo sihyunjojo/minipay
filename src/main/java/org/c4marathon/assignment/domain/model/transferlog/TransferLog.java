0384a7f28d55194006988f68425711deb4f8ef93
sihyunjojo <si4018@naver.com>
2025-05-14 16:51:55 +0900

    feat: accountNumber 추가

--- src/main/java/org/c4marathon/assignment/domain/model/transferlog/TransferLog.java
+++ src/main/java/org/c4marathon/assignment/domain/model/transferlog/TransferLog.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 import org.c4marathon.assignment.common.model.BaseTimeEntity;
 import org.c4marathon.assignment.domain.model.transfer.enums.TransferType;
 import org.c4marathon.assignment.domain.model.transfer.enums.TransferStatus;
-import org.c4marathon.assignment.domain.model.account.enums.AccountType;
 
 @Entity
 @Getter
@@ -27,11 +26,21 @@ public class TransferLog extends BaseTimeEntity {
 	@Column(nullable = true)
 	private Long parentTransactionId;
 
-	@Column(nullable = false)
-	private Long senderAccountId;
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name = "id", column = @Column(name = "from_account_id")),
+		@AttributeOverride(name = "type", column = @Column(name = "from_account_type")),
+		@AttributeOverride(name = "number", column = @Column(name = "from_account_number"))
+	})
+	private AccountSnapshot from;
 
-	@Column(nullable = false)
-	private Long receiverAccountId;
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name = "id", column = @Column(name = "to_account_id")),
+		@AttributeOverride(name = "type", column = @Column(name = "to_account_type")),
+		@AttributeOverride(name = "number", column = @Column(name = "to_account_number"))
+	})
+	private AccountSnapshot to;
 
 	@Column(nullable = false)
 	private long amount;
@@ -43,14 +52,6 @@ public class TransferLog extends BaseTimeEntity {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private TransferStatus status;
-
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private AccountType senderAccountType;
-
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private AccountType receiverAccountType;
 
 	@Column(nullable = false)
 	private LocalDateTime sendTime;
