f500121fc06dc7090bdf46d0c8cec9c54f530694
sihyunjojo <si4018@naver.com>
2025-05-15 20:45:34 +0900

    feat: enum 타입에 따른 송금 시간 결정 메서드 추가

--- src/main/java/org/c4marathon/assignment/domain/model/transfer/enums/TransferType.java
+++ src/main/java/org/c4marathon/assignment/domain/model/transfer/enums/TransferType.java
@@ -4,4 +4,20 @@ public enum TransferType {
 	IMMEDIATE,
 	PENDING,
 	CHARGE,
+	FIXED_TERM,
+	INTEREST;
+
+	public boolean isSenderTimeAuto(TransferStatus status) {
+		return switch (this) {
+			case IMMEDIATE, CHARGE, FIXED_TERM, INTEREST -> true;
+			case PENDING -> status == TransferStatus.PENDING;
+		};
+	}
+
+	public boolean isReceiverTimeAuto(TransferStatus status) {
+		return switch (this) {
+			case IMMEDIATE, CHARGE, FIXED_TERM, INTEREST -> true;
+			case PENDING -> status == TransferStatus.COMPLETED;
+		};
+	}
 }
