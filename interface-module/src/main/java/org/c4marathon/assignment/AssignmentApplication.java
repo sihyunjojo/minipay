1c3b074527c0703865cc14df2f752776386c2298
sihyunjojo <si4018@naver.com>
2025-06-11 20:36:42 +0900

    feat: property 정보 scan 기능 추가

--- interface-module/src/main/java/org/c4marathon/assignment/AssignmentApplication.java
+++ interface-module/src/main/java/org/c4marathon/assignment/AssignmentApplication.java
@@ -1,13 +1,18 @@
 package org.c4marathon.assignment;
 
+import org.c4marathon.assignment.infra.properties.MainAccountPolicy;
+import org.c4marathon.assignment.infra.properties.PendingTransferPolicyProperties;
+import org.c4marathon.assignment.infra.properties.SavingAccountPolicy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackageClasses = { MainAccountPolicy.class, SavingAccountPolicy.class, PendingTransferPolicyProperties.class})
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
