d56d71cdc17252d8d2d72e17e88b6dc35f977d8b
sihyunjojo <si4018@naver.com>
2025-06-11 18:36:55 +0900

    feat: mapper를 통한 DTO 변경 로직 추가

--- interface-module/src/main/java/org/c4marathon/assignment/usecase/settlement/SettlementUseCase.java
+++ interface-module/src/main/java/org/c4marathon/assignment/usecase/settlement/SettlementUseCase.java
@@ -2,9 +2,12 @@ package org.c4marathon.assignment.usecase.settlement;
 
 import java.util.List;
 
+import org.c4marathon.assignment.domain.command.SettlementCommand;
+import org.c4marathon.assignment.domain.model.Settlement;
 import org.c4marathon.assignment.domain.service.SettlementService;
 import org.c4marathon.assignment.api.settlement.dto.SettlementRequestDto;
 import org.c4marathon.assignment.api.settlement.dto.SettlementResponseDto;
+import org.c4marathon.assignment.usecase.settlement.mapper.SettlementMapper;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -14,8 +17,14 @@ import lombok.RequiredArgsConstructor;
 public class SettlementUseCase{
 
 	private final SettlementService settlementService;
+	private final SettlementMapper mapper;
 
 	public List<SettlementResponseDto> settle(SettlementRequestDto request) {
-		return settlementService.calculateSettlement(request);
+		SettlementCommand command = mapper.toCommand(request);
+		List<Settlement> settlements = settlementService.calculateSettlement(command);
+
+		return settlements.stream()
+			.map(settlement -> new SettlementResponseDto(settlement.getTargetMemberId(), settlement.getAmount()))
+			.toList();
 	}
 }
