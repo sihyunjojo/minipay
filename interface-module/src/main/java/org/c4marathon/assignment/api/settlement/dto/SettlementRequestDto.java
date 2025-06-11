66e2323f20b44a0adec1efa29781fc831953226e
sihyunjojo <si4018@naver.com>
2025-06-11 20:35:34 +0900

    refactor: 변수명 변경

--- interface-module/src/main/java/org/c4marathon/assignment/api/settlement/dto/SettlementRequestDto.java
+++ interface-module/src/main/java/org/c4marathon/assignment/api/settlement/dto/SettlementRequestDto.java
@@ -2,8 +2,8 @@ package org.c4marathon.assignment.api.settlement.dto;
 
 import java.util.List;
 
-import org.c4marathon.assignment.common.validation.NoDuplicateElements;
 import org.c4marathon.assignment.model.policy.SettlementPolicy;
+import org.c4marathon.assignment.validation.NoDuplicateElements;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
@@ -14,16 +14,16 @@ public record SettlementRequestDto(
 
 	@NotNull
 	@Schema(description = "정산 방식", defaultValue = "EQUAL", allowableValues = {"EQUAL", "RANDOM"})
-	SettlementPolicy type,
+	SettlementPolicy policyType,
 
 	@NotNull
-	@Positive(message = "정산 금액은 0보다 커야 합니다.") //----MODIFIED PART 1----
+	@Positive(message = "정산 금액은 0보다 커야 합니다.")
 	@Schema(description = "전체 정산 금액", example = "20000")
 	Long totalAmount,
 
 	@NotNull
 	@NotEmpty(message = "참여자 ID 목록은 비어 있을 수 없습니다.")
-	@NoDuplicateElements(message = "참여자 ID 목록에 중복된 값이 있습니다.") //----MODIFIED PART 2----
+	@NoDuplicateElements(message = "참여자 ID 목록에 중복된 값이 있습니다.")
 	@Schema(description = "참여자 ID 목록", example = "[1,2,3]")
 	List<Long> participantMemberIdList
 
