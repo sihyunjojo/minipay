276a0176a8deed5280a477ebd2addd1c7f80e894
sihyunjojo <si4018@naver.com>
2025-06-11 18:36:39 +0900

    refactor: 객체명 변경

--- settlement-module/src/main/java/org/c4marathon/assignment/domain/service/SettlementService.java
+++ settlement-module/src/main/java/org/c4marathon/assignment/domain/service/SettlementService.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.SplittableRandom;
 
+import org.c4marathon.assignment.domain.command.SettlementCommand;
+import org.c4marathon.assignment.domain.model.Settlement;
 import org.c4marathon.assignment.model.policy.ExternalAccountPolicy;
-import org.c4marathon.assignment.settlement.dto.SettlementRequestDto;
-import org.c4marathon.assignment.settlement.dto.SettlementResponseDto;
+import org.c4marathon.assignment.model.policy.SettlementPolicy;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,37 +15,38 @@ public class SettlementService {
 
 	public static final Long COMPANY_ID = ExternalAccountPolicy.COMPANY.getId();
 
-	public List<SettlementResponseDto> calculateSettlement(SettlementRequestDto request) {
-		List<Long> participantIds = request.participantMemberIdList();
-		long totalAmount = request.totalAmount();
+	public List<Settlement> calculateSettlement(SettlementCommand command) {
+		List<Long> participantMemberIdList = command.participantMemberIdList();
+		long totalAmount = command.totalAmount();
+		SettlementPolicy policyType = command.policyType();
 
-		return switch (request.type()) {
-			case SettlementPolicy.EQUAL -> calculateEqualShare(totalAmount, participantIds);
-			case SettlementPolicy.RANDOM -> calculateRandomShare(totalAmount, participantIds);
-			default -> throw new IllegalStateException(String.format("%s는 존재하지 않는 값입니다. ", request.type()));
+		return switch (policyType) {
+			case EQUAL -> calculateEqualShare(totalAmount, participantMemberIdList);
+			case RANDOM -> calculateRandomShare(totalAmount, participantMemberIdList);
+			default -> throw new IllegalStateException(String.format("%s는 존재하지 않는 값입니다. ", command.policyType()));
 		};
 	}
 
 	// 1/n 후 나머지 값은 타 페이 서비스와 같이 회사에서 지원하는 방식
-	private List<SettlementResponseDto> calculateEqualShare(long total, List<Long> participantMemberIdList) {
+	private List<Settlement> calculateEqualShare(long total, List<Long> participantMemberIdList) {
 		int count = participantMemberIdList.size();
 		long base = total / count;
 		long remainder = total % count;
 
-		List<SettlementResponseDto> responses = new ArrayList<>();
+		List<Settlement> responses = new ArrayList<>();
 		for (Long participantId : participantMemberIdList) {
-			responses.add(new SettlementResponseDto(participantId, base));
+			responses.add(new Settlement(participantId, base));
 		}
 
 		// 남은 금액은 회사(-1)에서 부담
 		if (remainder > 0) {
-			responses.add(new SettlementResponseDto(COMPANY_ID, remainder));
+			responses.add(new Settlement(COMPANY_ID, remainder));
 		}
 
 		return responses;
 	}
 
-	private List<SettlementResponseDto> calculateRandomShare(long total, List<Long> participantMemberIdList) {
+	private List<Settlement> calculateRandomShare(long total, List<Long> participantMemberIdList) {
 		int count = participantMemberIdList.size();
 		SplittableRandom rand = new SplittableRandom();
 		List<Long> shares = new ArrayList<>();
@@ -58,9 +60,9 @@ public class SettlementService {
 		}
 		shares.add(remaining);
 
-		List<SettlementResponseDto> responses = new ArrayList<>();
+		List<Settlement> responses = new ArrayList<>();
 		for (int i = 0; i < count; i++) {
-			responses.add(new SettlementResponseDto(participantMemberIdList.get(i), shares.get(i)));
+			responses.add(new Settlement(participantMemberIdList.get(i), shares.get(i)));
 		}
 
 		return responses;
